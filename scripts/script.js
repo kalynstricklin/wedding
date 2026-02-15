7dd62ece85d13a156078b56f67b1a1982828852d
kalynstricklin <kalynstricklin@botts-inc.com>
2026-02-15 11:45:39 -0600

    updated

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+
+const wedding_date = new Date('March 16, 2026 00:00:00').getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = wedding_date - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById('days').innerText = days > 0 ? days : 0;
+}
+
+function setActive(){
+    const navItems = document.querySelectorAll('.nav-item li');
+    navItems.forEach(item => item.addEventListener('click', function(){
+        const isActive = document.querySelector('.nav-item li.active');
+        if(isActive) isActive.classList.remove('active');
+
+        this.parentElement.classList.add('active');
+    }))
+    // navItems.forEach(item => item.classList.remove('active'));
+    // element.parentElement.classList.add('active');
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
+
+
